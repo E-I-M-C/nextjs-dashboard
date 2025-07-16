4da75897a2975170abc5114f10481877a9ba1d0b
E-I-M-C <mormons.at@gmail.com>
2025-07-16 14:21:03 -0700

    Made DeleteInvoice throw error

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -102,15 +102,15 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
 }
 
 export async function deleteInvoice(id: string) {
-  //throw new Error('Failed to Delete Invoice');
+  throw new Error('Failed to Delete Invoice');
 
-  try {
+  /*try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
   } catch (error) {
     return {
       message: 'Database Error: Failed to Delete Invoice.',
     };
-  }
+  }*/
 
   revalidatePath('/dashboard/invoices');
 }
