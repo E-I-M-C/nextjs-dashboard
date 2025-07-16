59bcacbe4dfdcafaab80e50cddb9756126b43bd8
E-I-M-C <mormons.at@gmail.com>
2025-07-16 14:08:29 -0700

    Inplemented code from chapters 6-16

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,20 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+//import Table from '@/app/ui/customers/table';
+//import { Suspense } from 'react';
+//import { fetchCustomers } from '@/app/lib/data';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default async function Page() {
+  //const customers = fetchCustomers();
+  //console.log(await customers);
+
+  return (<>
+    <p>Customers Page</p>
+    {/* <Suspense>
+      <Table customers={customers} />
+    </Suspense> */}
+  </>);
 }
